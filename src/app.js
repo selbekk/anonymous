f51884fb37c278152d8b2c32f48f0e55f272f2ce
selbekk <kristofer.selbekk@bekk.no>
2015-03-09 00:06:34 +0100

    Add basic API needed for frontpage

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+var entries = [];
 
+app.route('api')
+	.get('/entries', function(req, res) {
+		res.json(entries);
+	})
+	.post('/entries', function(req, res) {
+		res.status(201);
+	});
 
 app.listen(8000, function() {
 	console.log("listening on port 8000");
