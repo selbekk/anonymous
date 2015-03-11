36dcdc7060384359fac2862d49d7689bb0ac7522
selbekk <kristofer.selbekk@bekk.no>
2015-03-11 20:16:50 +0100

    Implement save entry

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var entries = [];
 
@@ -10,6 +12,8 @@ app.route('/api/entries')
 		res.json(entries);
 	})
 	.post(function(req, res) {
+        var entry = req.body;
+        entries.push(entry);
 		res.sendStatus(201);
 	});
 
